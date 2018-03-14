9821b190e76877c1f4c1581d15d6ced22f8136b1
William Antony <william@pathadan.com>
2018-03-14 15:38:12 -0500

    Completed executeProcess()

--- src/Process.js
+++ src/Process.js
@@ -31,12 +31,19 @@ class Process {
     // Also toggle its `this.stateChanged` property to `true`
     // Else, decrement this process's `this.cpuTimeNeeded` property by the input `time`
     executeProcess(time) {
+        this.stateChanged = false;
 
-   }
+        if (this.blockingTimeNeeded > 0) {
+            this.queue.emitInterrupt(this, SchedulerInterrupt.PROCESS_BLOCKED);
+            this.stateChanged = true;
+        } else {
+            this.cpuTimeNeeded -= time;
+        }
+    }
 
-   // Decrement this process's `this.blockingTimeNeeded` by the input `time`
-   // If `this.blockingTimeNeeded` is 0 or less, emit a queue interrupt nofifying 
-   // the process is ready and toggle `this.stateChanged` to `true`
+    // Decrement this process's `this.blockingTimeNeeded` by the input `time`
+    // If `this.blockingTimeNeeded` is 0 or less, emit a queue interrupt nofifying 
+    // the process is ready and toggle `this.stateChanged` to `true`
     executeBlockingProcess(time) {
 
     }
